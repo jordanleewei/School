d581716239993543be0f43f23ae4f5cfe0f64a05
Strryke <gavinong2000@gmail.com:git config --global color.ui truegit config --global user.name YOUR>
2021-11-14 16:32:51 +0800

    screen and snake setup movement food initialised

--- snake-game/snake.py
+++ snake-game/snake.py
@@ -1,29 +1,81 @@
-from turtle import *
+from turtle import Turtle, Screen
+import time
+from random import randrange
 
+# Screen setup
 screen = Screen()
-snake = Turtle()
+screen.title("Snake Game")
+screen.bgcolor("black")
+screen.tracer(0)
 
-resetscreen()
+# Snake setup
+start_positions = [(0,0), (-20,0), (-40,0)]
+segments = []
 
+for position in start_positions:
+    new = Turtle("square")
+    new.up()
+    new.color("white")
+    new.goto(position)
+    segments.append(new)
+    
+# Food
+def generate_rand():
+    new_location = (randrange(100), randrange(100))
+    for segment in segments:
+        if segment != new_location:
+            return new_location
+        else:
+            return generate_rand()
+
+def new_food():
+    food_position = generate_rand()
+    food = Turtle("circle")
+    food.up()
+    food.color("red")
+    food.setposition(food_position)
+    
+# Check_collision
+
+def check_collision(head, food):
+    if head.distance(food) < 20:
+        new_food()
+    
+
+# Movement of snake
 playing = True
 
-color('red', 'yellow')
-bgcolor('black')
-screensize(500,500)
-screen.delay(5)
-snake.speed(2)
+def move(segments):
+    for i in range(len(segments) - 1, 0, -1):
+        segments[i].setpos(segments[i-1].pos())
+    segments[0].forward(20)
+        
+    
+# Key bindings 
+def up(): segments[0].setheading(90)
+def down(): segments[0].setheading(270)
+def left(): segments[0].setheading(180)
+def right(): segments[0].setheading(0)
+    
+screen.onkey(up, "Up")
+screen.onkey(down, "Down")
+screen.onkey(left, "Left")
+screen.onkey(right, "Right")
 
-#key bindings to move snake
-screen.onkey(snake.setheading(90), "Up")
-screen.onkey(snake.setheading(180), "Right")
-screen.onkey(snake.setheading(270), "Down")
-screen.onkey(snake.setheading(0), "a")
 screen.listen()
 
-def game():
-    forward(1)    
+
+
+
+
+# Game loop
+new_food()
 
 while playing:
-    game()
+    screen.update()
+    move(segments)
+    time.sleep(0.1)
 
-done()
+
+
+screen.exitonclick()
